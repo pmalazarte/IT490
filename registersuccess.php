3cee8c05a5a8c9f9cd800486ddeb403f7ace54c4
pmalazarte <43541197+pmalazarte@users.noreply.github.com>
2018-11-02 14:55:57 -0400

    Add files via upload

---
+++ registersuccess.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Success</title>
+	<link rel="stylesheet" 
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="form">
+    	<h1>Successfully created your account</h1>
+    	<a href="index.html"><button class="btn btn-secondary btn-lg btn-block"/>Home</button></a>
+    </div>
+
+</body>
+</html>
