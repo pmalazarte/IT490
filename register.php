4677313eaa8d04f4547052a0b5c1de5e144aed5b
pmalazarte <43541197+pmalazarte@users.noreply.github.com>
2018-11-04 23:18:17 -0500

    fixed register.php
    unexpected ',' error

--- register.php
+++ register.php
@@ -7,12 +7,12 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $json = doRegistration($first_name, $last_name, $email, $password);
-$response = json_decode($json)
+$response = json_decode($json);
 echo "doRegistration : $response".PHP_EOL;
 
 if($response === false){
       echo "Registration failed, please try again";
-      error_log("ERROR: Registration failed, please try again "), 3, "/var/www/html/errorlog.log");
+      error_log("ERROR: Registration failed, please try again ", 3, "/var/www/html/errorlog.log");
       header("location:error.php");
 }
 else{
